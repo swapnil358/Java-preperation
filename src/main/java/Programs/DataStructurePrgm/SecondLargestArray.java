4d7666d2a2748ccc0849ec3842e852923ce8dceb
swapnil358 <swapnil.bodade88@gmail.com>
2023-09-11 17:48:06 +0530

    modified

--- src/main/java/Programs/DataStructurePrgm/SecondLargestArray.java
+++ src/main/java/Programs/DataStructurePrgm/SecondLargestArray.java
@@ -1,38 +1,53 @@
 package Programs.DataStructurePrgm;
 
 public class SecondLargestArray {
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
 
-		// https://youtu.be/R57n33zxgIw?list=PLlhM4lkb2sEiB1S_dHX8id1i_IN81t-q2
+        // https://youtu.be/R57n33zxgIw?list=PLlhM4lkb2sEiB1S_dHX8id1i_IN81t-q2
+        System.out.println("method1");
+        method1();
+        System.out.println("method2");
+        method2();
+    }
 
-		int[] a = { 9, 3, 5, 2, 8, 1 };
+    private static void method1() {
+        int[] a = {9, 3, 5, 2, 8, 1};
+        int temp;
 
-		int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] < a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("After swapping: " + a[i]);
+        }
+        System.out.println("SecondLargestArray: " + a[1]);
+    }
 
-		for (int i = 0; i < a.length; i++) {
+    private static void method2() {
+        int[] a = {9, 3, 5, 2, 8, 1};
+        int largest = a[0];
+        int secondLargest = a[0];
 
-			for (int j = i + 1; j < a.length; j++) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < largest) {
+                secondLargest = largest;
+                largest = a[i];
+            } else if (a[i] > secondLargest && a[i] < largest) {
+                secondLargest = a[i];
+            }
+        }
+        System.out.println("largest is: " + largest);
+        System.out.println("secondLargest is: " + secondLargest);
+    }
 
-				if (a[i] < a[j]) {
 
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-
-				}
-			}
-
-		}
-
-		for (int i = 0; i < a.length; i++) {
-			System.out.println("After swapping: " + a[i]);
-		}
-		System.out.println("SecondLargestArray: "+a[1]);
-	}
-
-	
-	}
+}
 
 
 
