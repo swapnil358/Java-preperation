30d1a2c830df9239971dd68a4e85fad845b38b75
swapnil358 <swapnil.bodade88@gmail.com>
2023-09-12 16:39:00 +0530

    modified

--- src/main/java/Programs/javaPrograme1/SwapNumber.java
+++ src/main/java/Programs/javaPrograme1/SwapNumber.java
@@ -31,15 +31,25 @@ public class SwapNumber {
 	         x = x + y; //15
 	         y = x - y; //10
 	         x = x - y; //5
-	         
-	         
-	         
-	         
-	         
-	         
 	         System.out.println("After swapping: " + " x is " + x + " and " + "y is "+ y);
-		
 
+
+		System.out.println("--------------------Swap string----------------------------");
+
+		String str1 = "Hello";
+		String str2 = "World";
+
+		System.out.println("Before swapping:");
+		System.out.println("str1: " + str1);
+		System.out.println("str2: " + str2);
+
+		str1 = str1 + str2;		//str1 = HelloWorld
+		str2 = str1.substring(0, str1.length() - str2.length());
+		str1 = str1.substring(str2.length());
+
+		System.out.println("\nAfter swapping:");
+		System.out.println("str1: " + str1);
+		System.out.println("str2: " + str2);
 	}
 
 }
