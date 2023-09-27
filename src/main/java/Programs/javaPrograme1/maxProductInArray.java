a038929ab0d5c71c0841f848def14cb7f8e3794f
swapnil358 <swapnil.bodade88@gmail.com>
2023-09-27 16:05:36 +0530

    pojo and max product

---
+++ src/main/java/Programs/javaPrograme1/maxProductInArray.java
@@ -0,0 +1,44 @@
+package Programs.javaPrograme1;
+
+
+//pgm to find the maximum product (i.e multiplication) in array
+
+public class maxProductInArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 7, 3, 2, 89};
+
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+        int lowest = Integer.MAX_VALUE;
+        int secondLowest = Integer.MAX_VALUE;
+
+
+        for (int num : arr) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest) {
+                secondHighest = num;
+            }
+            if (num < lowest) {
+                secondLowest = lowest;
+                lowest = num;
+            } else if (num < secondLowest) {
+                secondLowest = num;
+            }
+        }
+
+        System.out.println(highest);
+        System.out.println(secondHighest);
+        System.out.println(lowest);
+        System.out.println(secondHighest);
+        int product1 = highest * secondHighest;
+        int product2 = lowest * secondLowest;
+
+        System.out.println(Math.max(product1, product2));
+    }
+
+
+}
