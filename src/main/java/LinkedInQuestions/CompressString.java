31cf021ef684e43f77682d3a226c135f3e5f83ba
swapnil358 <swapnil.bodade88@gmail.com>
2023-10-08 10:27:08 +0530

    CompressString

--- src/main/java/LinkedInQuestions/CompressString.java
+++ src/main/java/LinkedInQuestions/CompressString.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class CompressString {
     public static void main(String[] args) {
-        String input = "aaabbccc";
+        String input = "aaabbcccd";
         String compressed = compressString(input);
         System.out.println("Compressed string: " + compressed);
     }
@@ -21,11 +21,16 @@ public class CompressString {
         char[] chars = s.toCharArray();
 
         for (char c : chars) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+         //   charCount.put(c, charCount.getOrDefault(c, 0) + 1);
             //charCount.getOrDefault(c, 0)
             // retrieves the current count of character c or returns 0
             // if it's not already in the HashMap. We then increment this count by 1
             // and update the value in the HashMap.
+            if (charCount.containsKey(c)) {
+                charCount.put(c,charCount.get(c)+1);
+            }else{
+                charCount.put(c, 1);
+            }
         }
 
         for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
