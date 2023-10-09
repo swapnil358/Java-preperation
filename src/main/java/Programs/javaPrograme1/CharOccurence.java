258e0cc861bed2b3e1bd2cb126ba71d891f0065f
swapnil358 <swapnil.bodade88@gmail.com>
2023-10-09 17:57:32 +0530

    modified

--- src/main/java/Programs/javaPrograme1/CharOccurence.java
+++ src/main/java/Programs/javaPrograme1/CharOccurence.java
@@ -5,37 +5,30 @@ import java.util.Set;
 
 public class CharOccurence {
 
-	public static void Char(String InputString) {
+    public static void main(String[] args) {
+        Char("abcd");
+    }
 
-		HashMap<Character, Integer> counter = new HashMap<Character, Integer>();
+    public static void Char(String InputString) {
+        HashMap<Character, Integer> counter = new HashMap<Character, Integer>();
+        char[] str = InputString.toCharArray();
 
-		char[] str = InputString.toCharArray();
+        for (char c : str) {
+            if (counter.containsKey(c)) {
+                counter.put(c, counter.get(c) + 1);
+            } else
+                counter.put(c, 1);
+        }
+        System.out.println(counter);
 
-		for (char c : str) {
-			if (counter.containsKey(c)) {
-				counter.put(c, counter.get(c) + 1);
-			} else
-				counter.put(c, 1);
-		}
+        Set<Character> SetString = counter.keySet();
+        System.out.println("Original string: " + InputString);
+        System.out.println("Dup char: " + " " + SetString);
 
-		System.out.println(counter);
-
-		Set<Character> SetString = counter.keySet();
-		System.out.println("Original string: " + InputString);
-		System.out.println("Dup char: " + " " + SetString);
-
-		for (Character s : SetString) {
-			if (counter.get(s) > 1) {
-				System.out.println(s + " : " + counter.get(s));
-
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-
-		Char("abcd");
-
-	}
-
+        for (Character s : SetString) {
+            if (counter.get(s) > 1) {
+                System.out.println(s + " : " + counter.get(s));
+            }
+        }
+    }
 }
