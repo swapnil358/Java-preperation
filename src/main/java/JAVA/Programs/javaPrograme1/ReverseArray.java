14390ba4fa74ddfe18609858f916ef747584d4f4
swapnil358 <swapnil.bodade88@gmail.com>
2023-12-09 19:13:57 +0530

    InterChangeFirstAndLastCharacterOfWord.java

---
+++ src/main/java/JAVA/Programs/javaPrograme1/ReverseArray.java
@@ -0,0 +1,23 @@
+package JAVA.Programs.javaPrograme1;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        //two pointer technique
+        
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
