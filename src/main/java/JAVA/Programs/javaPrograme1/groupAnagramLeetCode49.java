54802d393e724887dd276a8bfc016e6383f98fed
Swapnil Bodade <swapnil.bodade88@gmail.com>
2024-09-04 12:35:09 +0530

    Java question added

---
+++ src/main/java/JAVA/Programs/javaPrograme1/groupAnagramLeetCode49.java
@@ -0,0 +1,16 @@
+package JAVA.Programs.javaPrograme1;
+
+/*Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+*/
+
+public class groupAnagramLeetCode49 {
+
+
+}
