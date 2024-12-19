c057621ab3eda8efaa494bd94a2248acca4ef242
Swapnil Bodade <51831943+swapnil358@users.noreply.github.com>
2024-12-19 08:47:48 +0530

    Create MaangInterview.java

---
+++ src/main/java/JAVA/Programs/DataStructurePrgm/MaangInterview.java
@@ -0,0 +1,49 @@
+Interview Questions - Round 3 - Top Fintech in MAANG
+
+One of the most challenging interview experience till now for me! 
+
+↳ Hiring Position: SDET IV (SDET Lead)
+
+↳ Coding Problems: 
+
+#1 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+ 
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Difficulty Level: Medium as per LeetCode
+
+#2 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ 
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Difficulty Level: Easy as per LeetCode
+
+Note: Coding round 1 hr.
+
+Interview Questions: 
+
+1) Intro and Brief info about the TechStack Used
+2) How do you make sure 3rd party APIs like payment gateways are integrated for Test Automation Framework? 
+3) Do you prefer to automate it using UI or API and Why?
+4) Why did Amazon moved back to Monolithic architecture? Can you justify?
+5) When should use LomBok for POJO classes instead of Builder Pattern?
+6) Explain OAuth 2 working with respect to Web Server Applications?
+7) How do you make sure which Test Scenarios can be removed after each iteration of release for Regression Suite?
+8) If you are using Selenium WebDriver, How do you create a Test Automation Framework for a page having 700 unique webelements? 
+9) How to keep API Tests idempotent?
+10) If you use a application like makemytrip, what types of scenarios will you automate?
+11) Wrote a SQL Query using 3 Inner Joins (Data was given by the interviewer)
+12) Explain the use and implementation of Response & Request Spec in RestAssured? Also tell me why we need to create a Abstraction layer?
+13) How do you generate a token if it gets expired in 30mins for each Regression suite execution?
+14) Is it possible to execute tests using Docker-Compose in your local machine, If yes then How?
+
+-x-x-
